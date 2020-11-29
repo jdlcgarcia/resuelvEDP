ad93718ccf778b88d676de3ee2a94a329dbbc55c
Juan Garcia <jcruzgar@gmail.com>
2020-11-29 20:40:13 +0100

    Feature Segundo Grado: Entrada de datos para la ecuación cuadrática

---
+++ entrada2.py
@@ -0,0 +1,7 @@
+from model.cuadratica import Cuadratica
+
+ecuacion = Cuadratica()
+ecuacion.pedir_datos()
+print("La ecuación a resolver es "+ecuacion.imprimir())
+#ecuacion.resolver()
+#print("La solución es "+str(ecuacion.obtener_solucion()))
