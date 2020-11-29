08b4f3681cb6238090a79796583cd4ff0502be55
Juan Garcia <jcruzgar@gmail.com>
2020-11-29 20:21:58 +0100

    Feature Segundo Grado: tabla para instalación

--- install.py
+++ install.py
@@ -5,3 +5,9 @@ db.create('ecuacion', '''id INTEGER PRIMARY KEY,
                             coeficiente INTEGER NOT NULL,
                             termino_independiente INTEGER NOT NULL,
                             resultado REAL NULL''')
+db.create('ecuacion_segundo_grado', '''id INTEGER PRIMARY KEY,
+                            coeficiente_A INTEGER NOT NULL,
+                            coeficiente_B INTEGER NOT NULL,
+                            coeficiente_C INTEGER NOT NULL,
+                            resultado_1 REAL NULL,
+                            resultado_2 REAL NULL''')
